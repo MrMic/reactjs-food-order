f9d56cee51965f037b6543774118d3db0ccad6d7
MichaelChlon2 <michaelchlon2@gmail.com>
2022-03-08 11:16:49 +0100

    Added cart Modal 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
 
 function App() {
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
     </CartProvider>
-  );
+  )
 }
 
-export default App;
+export default App
